dbfb99ac467a582f6d82bbe430bed3de1d4f2d94
Luiz Alberto Ferreira Alberto <108027437+LopesObstinado@users.noreply.github.com>
2022-06-23 00:09:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var x = 123;
+
+x = 456;
+
+
+const y = "Luiz Alberto";
+
+console.log(x)
+console.log(y)
+
+//variável global
+    //var e const
+    //var recebe valores que     podem ser alterados;
+
+    //const  'imutavel'
+
+
+{
+let z = "Meu nome é:"
+  console.log(x)
+  console.log(z)
+  console.log(y)
+}
+
+//console.log(z)
+//variável let so pode ser usada dentro do bloco
